747e011768dcc2a84c12872c87dc735c64349bc8
fang.yang <yangfang2028@126.com>
2023-04-05 14:36:16 +0800

    ✨Example printing.

---
+++ print/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+	fmt.Print("Hello yang")
+	fmt.Println("Hello fang")
+}
+
+//func main() {
+//	fmt.Printf("Hello, my name is %s, Today is %d. The weather is good, which is %t", "yang fang", 5, false)
+//}
